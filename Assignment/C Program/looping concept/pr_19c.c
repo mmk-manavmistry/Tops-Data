9241bbd8fe168a4eccee432864f66f075c9b148c
mmk-manavmistry <126792273+mmk-manavmistry@users.noreply.github.com>
2024-08-10 10:44:37 +0530

    my program

--- Assignment/C Program/looping concept/pr_19c.c
+++ Assignment/C Program/looping concept/pr_19c.c
@@ -8,7 +8,14 @@
 #include<stdio.h>
 void main(){
     int row,col;
-    for(row=1<=5;row++){
-        for()
+    for(row=1;row<=5;row++){
+        for(col=1;col<=9;col++){
+            if(col>=6-row && col<=4+row){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
     }
 }
