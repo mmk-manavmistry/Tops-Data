de226c73bb82a78ccab2d6d25968f5b517c32680
mmk-manavmistry <126792273+mmk-manavmistry@users.noreply.github.com>
2024-10-15 11:40:43 +0530

    oop prog

--- Assignment/Cpp Program/inheritance polymorphism/pr12.cpp
+++ Assignment/Cpp Program/inheritance polymorphism/pr12.cpp
@@ -1,38 +1,69 @@
 // Write a program to swap the two numbers using friend function without using third variable
 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// class num1; 
+
+// class num2 {
+//     private:
+//     int a;
+//         a=10;
+        
+//     public:
+//         friend int swap(num2, num1);
+// };
+
+// class num1 {
+//     private:
+        
+//     int b;
+//         b=20;
+//     public:
+//         friend int swap(num2, num1);
+// };
+
+// int swap(num2 n2, num1 n1) {
+//     n1=n1+n2;
+//     n2=n1-n2;
+//     n1=n1-n2;
+// }
+
+// int main() {
+//     num2 tt;
+//     num1 mm;
+//     cout << "a= " <<swap(num2,num1)<< endl;
+//     cout << "b= " <<swap(num2,num1)<< endl;
+
+// }
+
+#include <iostream>
 using namespace std;
 
-class num1; 
-
-class num2 {
-    private:
+class B
+{   
+    public:
     int a;
-        a=10;
-        
-    public:
-        friend int swap(num2, num1);
-};
-
-class num1 {
-    private:
-        
     int b;
-        b=20;
-    public:
-        friend int swap(num2, num1);
-};
 
-int swap(num2 n2, num1 n1) {
-    n1=n1+n2;
-    n2=n1-n2;
-    n1=n1-n2;
+    void re() {
+        cout << "Enter a & b:";
+        cin >> a >> b;
+    }
+
+    friend void swap(B& obj);  
+}; 
+
+void swap(B& obj) {
+    int temp = obj.a;
+    obj.a = obj.b;
+    obj.b = temp;
+    cout << "After swap: a: " << obj.a << ", b: " << obj.b << endl;
 }
 
 int main() {
-    num2 tt;
-    num1 mm;
-    cout << "a= " <<swap(num2,num1)<< endl;
-    cout << "b= " <<swap(num2,num1)<< endl;
-
+    B b;
+    cout << "Before swap: ";
+    b.re();
+    swap(b);
 }
